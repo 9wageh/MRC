a1e06002991a2f5106bf69f83cc0f66e530b247e
Yousef Wageh <wagehwork001@gmail.com>
2025-01-24 20:29:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let amountInput = document.getElementById("mortgageAmount");
+let termInput = document.getElementById("mortgageTerm");
+let rateInput = document.getElementById("mortgageRate");
+let repaymentCheckbox = document.getElementById("checkbox-1");
+let interestOnlyCheckbox = document.getElementById("checkbox-2");
+let contentRight = document.querySelector(".contentRight");
+let contentRightTwo = document.querySelector(".contentRight-2");
+let totalOne = document.getElementById("total-1");
+let totalTwo = document.getElementById("total-2");
+
+function calc() {
+    let amount = parseFloat(amountInput.value);
+    let term = parseFloat(termInput.value);
+    let rate = parseFloat(rateInput.value);
+    if (isNaN(amount) || isNaN(term) || isNaN(rate)) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Write Somthing!",
+        });
+
+    } else {
+        let months = term * 12;
+        let monthlyRate = (rate / 100) / 12;
+        let power = Math.pow(1 + monthlyRate, months);
+        let numerator = monthlyRate * power;
+        let denominator = power - 1;
+        contentRight.style.display = "none";
+        contentRightTwo.style.display = "block";
+        if (repaymentCheckbox.checked) {
+            let monthlyPayment = amount * (numerator / denominator);
+            totalOne.innerHTML = (monthlyPayment.toFixed(2)).toLocaleString('en-US');
+            totalTwo.innerHTML = (monthlyPayment.toFixed(2) * term * 12).toLocaleString('en-US');
+        } else if (interestOnlyCheckbox.checked) {
+            let monthlyPayment = amount * monthlyRate;
+            totalOne.innerHTML = (monthlyPayment.toFixed(2)).toLocaleString('en-US');
+            totalTwo.innerHTML = (monthlyPayment.toFixed(2) * term * 12).toLocaleString('en-US');
+        } else {
+            window.alert("Please select a mortgage type.");
+        }
+    }
+}
+// clear all tn
+function clearAll() {
+    amountInput.value = " ";
+    termInput.value = " ";
+    rateInput.value = " ";
+    if (repaymentCheckbox.checked || interestOnlyCheckbox.checked) {
+        repaymentCheckbox.checked = false;
+        interestOnlyCheckbox.checked = false;
+    }
+    totalOne.innerHTML = "0,00.00";
+    totalTwo.innerHTML = "000,000.0";
+}
---
+++ style-guide.md
@@ -0,0 +1,63 @@
+
+## Colors
+
+### Primary
+
+- Lime: hsl(61, 70%, 52%)
+- Red: hsl(4, 69%, 50%)
+
+### Neutral
+
+- White: hsl(0, 0%, 100%)
+- Slate 100: hsl(202, 86%, 94%)
+- Slate 300: hsl(203, 41%, 72%)
+- Slate 500: hsl(200, 26%, 54%)
+- Slate 700: hsl(0, 0.00%, 0.40%)
+- Slate 900: hsl(202, 55%, 16%)
+
+## Typography
+
+### Body Copy
+
+- Font size (paragraph): 16px 
+
+### Font
+
+- Family: [Plus Jakarta Sans](https://fonts.google.com/specimen/Plus+Jakarta+Sans)
+- Weights: 500, 700
+
+### Text
+Mortgage Calculator
+  Clear All
+
+  Mortgage Amount
+
+  Mortgage Term
+
+  Interest Rate
+
+  Mortgage Type
+  Repayment
+  Interest Only
+
+  Calculate Repayments
+
+  <!-- Empty results start -->
+
+  Results shown here
+
+  Complete the form and click “calculate repayments” to see what 
+  your monthly repayments would be.
+
+  <!-- Empty results end -->
+
+  <!-- Completed results start -->
+
+  Your results
+
+  Your results are shown below based on the information you provided. 
+  To adjust the results, edit the form and click “calculate repayments” again.
+
+  Your monthly repayments
+
+  Total you'll repay over the term
